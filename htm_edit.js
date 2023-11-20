3c9dc522a2403e483b5f793c1c40cbfae85d9ddc
hoodaatwa <31570775+hoodaatwa@users.noreply.github.com>
2023-11-20 13:46:59 +0200

    Update htm_edit.js

--- htm_edit.js
+++ htm_edit.js
@@ -1,21 +1,38 @@
-$(document).ready(()=>{
-    $('#ri2').change(function(){
-        var x=$('#ri2').val();
-        $('.ru').html(x);
-    })
-    $('.run').click(()=>{
-        $('.thatdisappear').css({"display":"block","height":"100%","position":"fixed","top":"0","width":"100%","left":"0","background-color":"#f7f7f7"});
-        $('.closebtn').fadeIn();
-    })
-    $('.clear').click(()=>{
-        $('.ru,#ri2').html('');
-        $('#ri2').val('');
-    })
-    $('div.closebtn').click(400,()=>{
-        $('.thatdisappear').slideUp();
-        $('.closebtn').fadeOut();
-    })
-    $('.copy').click(function(){
-        navigator.clipboard.writeText($('#ri2').val());
-    })
-});
+
+    $(document).ready(() => {
+        $('.run').click(() => {
+            try {
+                var x = $('#ri2').val();
+                var result = eval(x);
+                $('#ri2').val(result);
+            } catch (error) {
+                $('#ri2').val('Error: ' + error.message);
+            }
+
+            // Display the output in the textarea and handle the UI changes
+            $('.thatdisappear').css({
+                "display": "block",
+                "height": "100%",
+                "position": "fixed",
+                "top": "0",
+                "width": "100%",
+                "left": "0",
+                "background-color": "#f7f7f7"
+            });
+            $('.closebtn').fadeIn();
+        });
+
+        $('.clear').click(() => {
+            $('.ru, #ri2').html('');
+            $('#ri2').val('');
+        });
+
+        $('div.closebtn').click(400, () => {
+            $('.thatdisappear').slideUp();
+            $('.closebtn').fadeOut();
+        });
+
+        $('.copy').click(() => {
+            navigator.clipboard.writeText($('#ri2').val());
+        });
+    });
